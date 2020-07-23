f6715dace4fd5b08d7721d2924b8bf3060df4799
Roberto Rocha <robrs.web@gmail.com>
2020-07-23 12:42:08 -0300

    Adicionando migrations

---
+++ database/migrations/2020_07_23_013048_create_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->string('cliente_nome');
+            $table->integer('cliente_id');
+            $table->string('codigo_pedido');
+            $table->float('valor_total');
+            $table->integer('quantidade_itens');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
---
+++ database/migrations/2020_07_23_014023_create_itens_pedido_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItensPedidoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itens_pedido', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pedido_id');
+            $table->integer('produto_id');
+            $table->float('valor');
+            $table->integer('quantidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itens_pedido');
+    }
+}
